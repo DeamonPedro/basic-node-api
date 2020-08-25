8602e9286f66fa58beb314e2b763bf33505bf72a
DeamonPedro <pedro.lucas.root@gmail.com>
2020-08-24 22:43:34 -0300

    refactor(Security): hash passwords in DB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const ratelimit = require("koa-ratelimit");
 const koaBody = require("koa-body");
 const jwt = require("koa-jwt");
 const Koa = require("koa");
@@ -7,7 +8,24 @@ const { AUTHENTICATION_ERROR } = require("./errors");
 const auth = require("./auth");
 
 const app = new Koa();
+const db = new Map();
 
+app.use(
+  ratelimit({
+    driver: "memory",
+    db: db,
+    duration: 60000,
+    errorMessage: "too many requests",
+    id: (ctx) => ctx.ip,
+    headers: {
+      remaining: "Rate-Limit-Remaining",
+      reset: "Rate-Limit-Reset",
+      total: "Rate-Limit-Total",
+    },
+    max: 100,
+    disableHeader: false,
+  })
+);
 app.use(koaBody());
 app.use((ctx, next) => {
   return next().catch((err) => {
